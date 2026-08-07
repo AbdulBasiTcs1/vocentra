a044e47d503593ce339e3eb44ee1f85d86f029de
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-08-07 09:55:05 +0500

    feat: initialize project structure with React client, Express server, and Tailwind CSS configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
     return (
-        <div>
-            <h1>Vocentra</h1>
-        </div>
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </>
     );
 }
 
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,7 +2,6 @@ import express from "express"
 import { googleAuth, logOut } from "../controllers/auth.controller.js"
 
 const router = express.Router()
-const PORT = process.env.PORT;
 
 router.post("/google", googleAuth)
 router.get("/logout", logOut)
